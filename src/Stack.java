5dcfd64bfaad12771b5ad4d25bdcb10bd028a356
gabrigoca <36449282+gabrigoca@users.noreply.github.com>
2018-02-21 17:53:55 +0100

    Afegeixo PilaBuidaException

--- src/Stack.java
+++ src/Stack.java
@@ -2,7 +2,7 @@
  * Created by jordi on 21/02/2018.
  */
 public interface Stack<T> {
-    public void push(T t);
-    public T pop();
+    public void push(T t) throws PilaPlenaException;
+    public T pop() throws PilaBuidaException;
     public int size();
 }
