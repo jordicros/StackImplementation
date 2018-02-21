6b990b4d4abe3ed3b225d5dccf8a07bf3bd65826
jordicros <36449492+jordicros@users.noreply.github.com>
2018-02-21 17:55:06 +0100

    interface

--- src/Stack.java
+++ src/Stack.java
@@ -2,9 +2,7 @@
  * Created by jordi on 21/02/2018.
  */
 public interface Stack<T> {
-
-
-    T pop();
+    T pop() throws PilaBuidaException;
      int size();
-    void push(T t);
+    void push(T t) throws PilaPlenaException;
 }
